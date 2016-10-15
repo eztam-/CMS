c0eff6b0dc94a173fca5397ebdedb3f2517b7184
x <x@x.com>
2016-10-15 18:20:03 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+var express     = require( 'express' ),
+    app         = express(),
+    nunjucks    = require( 'nunjucks' ) ;
+
+// Define port to run server on
+var port = process.env.PORT || 9000 ;
+
+// Configure Nunjucks
+var _templates = process.env.NODE_PATH ? process.env.NODE_PATH + '/templates' : 'templates' ;
+nunjucks.configure( _templates, {
+    autoescape: true,
+    cache: false,   // Set true in production
+    express: app,
+    watch: true     // Set true in production
+} ) ;
+// Set Nunjucks as rendering engine for pages with .html suffix
+app.engine( 'html', nunjucks.render ) ;
+app.set( 'view engine', 'html' ) ;
+
+// Directory with static files
+app.use(express.static('node_modules'));
+
+// Default landing page
+// TODO The default page should be set as constant anywhere
+app.get( '/', function( req, res ) {
+    res.render('home.html', {currentPage: 'home'});
+} ) ;
+
+// Respond to all GET requests by rendering relevant page using Nunjucks
+app.get( '/:page', function( req, res ) {
+    res.render( req.params.page+'.html', {currentPage: req.params.page});
+} ) ;
+
+// Start server
+app.listen( port ) ;
+console.log( 'Server running at http://localhost:%s', port ) ;
