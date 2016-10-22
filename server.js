0cf951b83a086e3f05d5d3b359fc33f08820735d
eztam <x@x.com>
2016-10-22 19:27:37 +0200

    Authentication

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var express     = require( 'express' ),
     nunjucks    = require( 'nunjucks' ),
     bodyParser  = require( 'body-parser' ),
     fs          = require('fs'),
-    cheerio     = require('cheerio');
+    cheerio     = require('cheerio'),
+    passport    = require('passport'),
+    Strategy    = require('passport-local').Strategy;
+var db = require('./db');
 
-// Define port to run server on
-var port = process.env.PORT || 9000 ;
+
+var port = process.env.PORT || 9000 ;       // Define port to run server on
 
 // Configure Nunjucks
 var _templates = process.env.NODE_PATH ? process.env.NODE_PATH + '/templates' : 'templates' ;
@@ -20,12 +23,69 @@ nunjucks.configure( _templates, {
 app.engine( 'html', nunjucks.render ) ;
 app.set( 'view engine', 'html' ) ;
 
-// Directory with static files
-app.use(express.static('static'));
+app.use(express.static('static'));          // Directory with static files
 app.use(express.static('node_modules'));
+app.use(bodyParser.json())                  // Use JSON format for request body mapping
 
-// Use JSON for request body mapping
-app.use(bodyParser.json())
+
+
+
+
+// Configure the authentication strategy for Passport. With the `verify` function
+// which receives the credentials (username and password) submitted by the user.
+// After authenticating an user, cb is called with an user object
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
+
+
+
+  // Configure Passport authenticated session persistence.
+  //
+  // In order to restore authentication state across HTTP requests, Passport needs
+  // to serialize users into and deserialize users out of the session.  The
+  // typical implementation of this is as simple as supplying the user ID when
+  // serializing, and querying the user record by ID from the database when
+  // deserializing.
+  passport.serializeUser(function(user, cb) {
+      cb(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, cb) {
+    db.users.findById(id, function (err, user) {
+      if (err) { return cb(err); }
+      cb(null, user);
+    });
+  });
+
+
+// Initialize Passport and restore authentication state from the session.
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.post('/login',
+    passport.authenticate('local', { failureRedirect: '/home' }),
+        function(req, res) {
+            res.redirect('/'); // TODO externalize post login page
+});
+
+
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/'); // TODO externalize post logout page
+});
+
+
+
 
 // Default landing page
 // TODO The default page should be set as constant anywhere
@@ -35,7 +95,11 @@ app.get( '/', function( req, res ) {
 
 // Respond to all GET requests by rendering relevant page using Nunjucks
 app.get( '/:page', function( req, res ) {
-    res.render( req.params.page+'.html', {currentPage: req.params.page});
+    res.render( req.params.page+'.html', {
+        currentPage: req.params.page,
+        isAuth: !!req.user
+    });
+    console.log("USER", req.user);
 } ) ;
 
 app.put( '/:page', function( req, res ) {
